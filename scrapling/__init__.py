eaf9d4f8f214674b6c6ed1ccadc2d5070f9cc88e
Payar Ahmadi <payar84@users.noreply.github.com>
2026-05-12 15:54:29 +0800

    Add __author_email__ placeholder for packaging tool compatibility

--- scrapling/__init__.py
+++ scrapling/__init__.py
@@ -9,12 +9,14 @@ Personal fork notes:
 - Added convenience alias: use `AutoFetcher` as shorthand for `StealthyFetcher`
 - Added `__version_info__` tuple for easier version comparisons
 - Added `NimbusParser` as alias for `Page` (my preferred name for the parser)
+- Added `__author_email__` for completeness
 - See upstream: https://github.com/D4Vinci/Scrapling
 """
 
 __version__ = "0.2.9"
 __version_info__ = tuple(int(x) for x in __version__.split("."))
 __author__ = "D4Vinci"
+__author_email__ = ""  # not listed upstream; placeholder for packaging tools
 __license__ = "MIT"
 
 from scrapling.core.fetchers import (
@@ -45,4 +47,5 @@ __all__ = [
     "StorageSystem",
     "__version__",
     "__version_info__",
+    "__author_email__",
 ]
